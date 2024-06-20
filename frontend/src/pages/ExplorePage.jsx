a393d126ac3272b2b1713ce4eb8b14909f763fcf
syedmaazsaeed <syedmaazsaeed@gmail.com>
2024-06-20 11:48:15 +0500

    Pushing UI design part into Remote

---
+++ frontend/src/pages/ExplorePage.jsx
@@ -0,0 +1,50 @@
+const ExplorePage = () => {
+  return (
+    <div className="px-4">
+      <div className="bg-glass max-w-2xl mx-auto rounded-md p-4">
+        <h1 className="text-xl font-bold text-center">
+          Explore Well-Known Repositories
+        </h1>
+        <div className="flex flex-wrap gap-2 my-2 justify-center">
+          <img
+            src="/html.svg"
+            alt="html logo"
+            className="h-11 sm:h-20 cursor-pointer"
+          />
+          <img
+            src="/css.svg"
+            alt="css logo"
+            className="h-11 sm:h-20 cursor-pointer"
+          />
+          <img
+            src="/javascript.svg"
+            alt="JavaScript logo"
+            className="h-11 sm:h-20 cursor-pointer"
+          />
+          <img
+            src="/typescript.svg"
+            alt="TypeScript logo"
+            className="h-11 sm:h-20 cursor-pointer"
+          />
+          <img
+            src="/c++.svg"
+            alt="C++ logo"
+            className="h-11 sm:h-20 cursor-pointer"
+          />
+          <img
+            src="/python.svg"
+            alt="Python logo"
+            className="h-11 sm:h-20 cursor-pointer"
+          />
+          <img
+            src="/java.svg"
+            alt="Java logo"
+            className="h-11 sm:h-20 cursor-pointer"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExplorePage;
